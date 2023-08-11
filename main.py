9a8bfdae9cd75d7ac997a475d2b3f16b4cf60ea1
VisaalPathak <visaalpathak@gmail.com>
2023-08-11 20:10:55 +0545

    refactored main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-from src.mailer import send_mail
+from src.mailer import Mailer
 
-send_mail()
+if __name__=="__main__":
+    mailer = Mailer()
+    mail = mailer.create_mail()
+    for user in mailer.receiver:
+        mailer.send_mail(receiver=user,msg=mail)
