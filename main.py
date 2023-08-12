bc0a5c43213af9671be7a57c25a28cec8c865ef0
VisaalPathak <visaalpathak@gmail.com>
2023-08-12 09:45:55 +0545

    updated main to send file

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from src.mailer import Mailer
 if __name__=="__main__":
     mailer = Mailer()
     mail = mailer.create_mail()
-    for user in mailer.receiver:
-        mailer.send_mail(receiver=user,msg=mail)
+    file_path = "./data/dummy.txt"
+    mail = mailer.attach_files(file_path,mail)
+    mailer.send_mail(receiver=mailer.receiver,msg=mail)
