e9384e2c02d71b846d5337cc9a08e27b27b8b1f2
VisaalPathak <visaalpathak@gmail.com>
2023-08-12 09:58:20 +0545

    updated main to send multiple files from a directory

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from src.mailer import Mailer
 if __name__=="__main__":
     mailer = Mailer()
     mail = mailer.create_mail()
-    file_path = "./data/dummy.txt"
-    mail = mailer.attach_files(file_path,mail)
+    file_dir = "./data/"
+    mail = mailer.attach_files(file_dir,mail)
     mailer.send_mail(receiver=mailer.receiver,msg=mail)
